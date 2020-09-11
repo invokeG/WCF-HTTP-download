a7f6ec37a60aa568f5ff8ba8d50e6775b425f13e
AmazingG_glb <1395362505_glb@qq.com>
2020-09-11 21:25:15 +0800

    添加项目文件。

---
+++ CSharpLab_4/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using CSharpLab_4.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSharpLab_4
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShowThis();
+        }
+        string[] fileInfo;
+        public void ShowThis()
+        {
+            Service1Client client = new Service1Client();
+            fileInfo = client.GetFilesInfo();
+            for(int i = 0; i < fileInfo.Length; i++)
+            {
+                string[] newstr = fileInfo[i].Split(',');
+                listbox.Items.Add("文件名："+newstr[0]+",文件大小："+newstr[1]+"B");
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            tb1.Text = "";
+            tb2.Text = "";
+            string[] ss = fileInfo[listbox.SelectedIndex].Split(',');
+            tb1.Text = "正在下载："+ss[0];
+            Service1Client client = new Service1Client();
+            Stream stream = client.DownloadStrem(ss[0]);
+            string filePath = System.IO.Path.Combine(@"E:\", ss[0]); 
+            FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+            byte[] bytes = new byte[1024];
+            int num;
+            int count0 = 0;
+            do
+            {
+                num = stream.Read(bytes, 0, bytes.Length);
+                fs.Write(bytes, 0, num);
+                count0 += num;
+            } while (num > 0);
+            fs.Close();
+            stream.Close();
+            tb2.Text = "下载完成，共下载"+count0+"字节，文件保存到"+filePath;
+        }
+    }
+}
---
+++ MyService/Service1.svc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MyService
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“Service1”。
+    // 注意: 为了启动 WCF 测试客户端以测试此服务，请在解决方案资源管理器中选择 Service1.svc 或 Service1.svc.cs，然后开始调试。
+    public class Service1 : IService1
+    {
+        public static List<string> filesInfo;
+        public static string path;
+        public List<string> GetFilesInfo()//获取文件列表信息
+        {
+            if(filesInfo==null)//只在调第一次时获取
+            {
+                filesInfo = new List<string>();
+                //获得路径信息，可执行文件目录与Foler文件夹进行拼接
+                path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Folder");
+                //构造DirectoryInfo对象，获得路径信息
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
+                //用GetFiles方法获得所有文件路径信息
+                FileInfo[] q = directoryInfo.GetFiles();
+                foreach (FileInfo v in q)
+                {
+                    filesInfo.Add(string.Format("{0},{1}",v.Name,v.Length));
+                }
+            }
+            return filesInfo;
+        }
+
+        public Stream DownloadStrem(string fileName)//返回基类的Stream
+        {
+            //用文件名拼出来全文件路径信息
+            string filePath = System.IO.Path.Combine(path, fileName);
+            try
+            {
+                //构造FileStream获取文件流对象也可以
+                FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                return fs;
+                //FileStream imageFile = File.OpenRead(filePath);
+                //return imageFile;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
